26ae271f4c1bf048a3f89a9084749be09d48cc81
jbosegre <jbose@mail.greenriver.edu>
2019-01-08 00:11:09 -0500

    rework the index page and added a functioning name print via order button

--- Cupcake.php
+++ Cupcake.php
@@ -10,10 +10,9 @@ $flavor = array("grasshopper"=>"The Grasshopper", "maple"=>"Whiskey Maple Bar",
     "carrot"=>"Carrot Walnut", "caramel"=>"Salted Caramel", "velvet"=>"Red Velvet",
     "lemon"=>"Lemon Drop", "tiramisu"=>"Tiramisu");
 
-function printCupcakeFlavors()
+function printCupcakeFlavors($flavor)
 {
-    global $flavor;
    foreach($flavor as $taste => $description) {
-       echo "<input type=\"checkbox\" name=\"".$flavor."\" value=\"".$taste."\"> $description<br>";
+       echo $description."<br>";
    }
 }
--- index.php
+++ index.php
@@ -2,20 +2,53 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge"><meta charset="UTF-8">
     <title>Cupcake</title>
 </head>
 <body>
     <h1>Create Your cupcake</h1>
-    <form action="Cupcake.php">
-        <label> Name: <input type="text" name="name"></label><br><br>
-        <label><input type="submit" value="Order"></label>
-        <pre>
-            <?php include 'Cupcake.php';
-
-            printCupcakeFlavors();
-            ?>
-        </pre>
-
+    <form action="" method="post">
+        <label> Name: <input type="text" name="username"></label><br><br>
+        <label>
+            <input type="checkbox" name="flavor[]" value="grasshopper">
+            The Grasshopper<br>
+            <input type="checkbox" name="flavor[]" value="maple">
+            Whiskey Maple Bar<br>
+            <input type="checkbox" name="flavor[]" value="carrot">
+            Carrot Walnut<br>
+            <input type="checkbox" name="flavor[]" value="caramel">
+            Salted Caramel<br>
+            <input type="checkbox" name="flavor[]" value="velvet">
+            Red Velvet<br>
+            <input type="checkbox" name="flavor[]" value="lemon">
+            Lemon Drop<br>
+            <input type="checkbox" name="flavor[]" value="tiramisu">
+            Tiramisu<br><br>
+        </label>
+        <label><input type="submit" value="Order"></label><br><br>
     </form>
+
+
+        <?php
+        if(!empty($_POST)) {
+
+            include "Cupcake.php";
+
+            if(!empty($_POST)){
+                echo "Thank you, ".$_POST["username"].", for your order!";
+                if(!empty($_POST["flavor"])){
+
+                    foreach($_POST['flavor[]'] as $flavor){
+                        printCupcakeFlavors($flavor);
+                    }
+                }
+            }
+            else{
+                echo "Error: name is missing";
+            }
+        }
+        ?>
 </body>
 </html>
